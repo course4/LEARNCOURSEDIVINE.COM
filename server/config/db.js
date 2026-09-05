8fc9cd88caa97ed61fe97a6f2dccd592c1a38e2c
akasheswar7 <doddiakash143@gmail.com>
2026-09-05 11:46:36 +0530

    Force IPv4 DNS resolution and family 4 in db.js to fix Render MongoDB Atlas connection timeouts

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dns = require('dns');
 
+// Force IPv4 first to prevent Render IPv6 DNS lookup timeouts on MongoDB Atlas
+try {
+  if (dns.setDefaultResultOrder) {
+    dns.setDefaultResultOrder('ipv4first');
+  }
+} catch (e) {}
+
 // Set reliable public DNS on Windows to prevent querySrv ECONNREFUSED on MongoDB Atlas
 if (process.platform === 'win32') {
   try {
@@ -8,14 +15,15 @@ if (process.platform === 'win32') {
   } catch (e) {}
 }
 
-const ATLAS_URI = 'mongodb+srv://info_db_user:8iTiFByhIMWvkPfx@cluster0.2be4pxm.mongodb.net/coursedivine';
+const ATLAS_URI = 'mongodb+srv://info_db_user:8iTiFByhIMWvkPfx@cluster0.2be4pxm.mongodb.net/coursedivine?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDB = async () => {
   const primaryUri = process.env.MONGODB_URI;
   const options = {
     serverSelectionTimeoutMS: 5000,
     maxPoolSize: 10,
-    socketTimeoutMS: 45000
+    socketTimeoutMS: 45000,
+    family: 4
   };
 
   // Try primary URI if specified and different from ATLAS_URI
